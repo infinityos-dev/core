34ff14dc9543832cf36f2e031ca6ed3949095e74
Viktor Popp <viktorpopp.dev@gmail.com>
2025-01-10 12:46:56 +0100

    Added more tests and renamed project to `infinity_os`

--- src/main.rs
+++ src/main.rs
@@ -24,21 +24,54 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+//#[test_case]
+//fn test_println_output() {
+//    let s = "Some test string that fits on a single line";
+//    println!("{}", s);
+//    for (i, c) in s.chars().enumerate() {
+//        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+//        assert_eq!(char::from(screen_char.ascii_character), c);
+//    }
+//}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
