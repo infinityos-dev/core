606718c31f2543adbe1057a021df09b851dcdc5a
Viktor Popp <viktorpopp.dev@gmail.com>
2025-01-11 17:45:25 +0100

    Added basic keybaord handler
    Tab and backspace does still not work

--- src/arch/amd64/interrupts.rs
+++ src/arch/amd64/interrupts.rs
@@ -1,4 +1,5 @@
 use crate::arch::gdt;
+use crate::print;
 use crate::println;
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
@@ -60,9 +61,51 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use lazy_static::lazy_static;
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                layouts::Us104Key,
+                HandleControl::Ignore
+            ));
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!(
+                    "{}",
+                    match character {
+                        ' ' => ' ', // Print space for spacebar
+                        _ => character,
+                    }
+                ),
+                DecodedKey::RawKey(key) => {
+                    match key {
+                        pc_keyboard::KeyCode::LControl |
+                        pc_keyboard::KeyCode::RControl |
+                        pc_keyboard::KeyCode::LAlt |
+                        pc_keyboard::KeyCode::RShift |
+                        // Add other non-printable keys here
+                        _ => (), // Do nothing for non-printable keys
+                    }
+                }
+            }
+        }
+    }
+
     unsafe {
         PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
 
