dbf95282fca9ffcdc21b1cfd8d791473f31d39fd
Viktor Popp <viktorpopp.dev@gmail.com>
2025-01-16 12:43:10 +0100

    Basic paging support

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -2,5 +2,6 @@ pub mod clock;
 pub mod gdt;
 pub mod interrupts;
 pub mod layouts;
+pub mod memory;
 pub mod string;
 pub mod vga;
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,38 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use infinity_os::kernel;
 use infinity_os::print;
 use infinity_os::user::shell;
+use x86_64::{structures::paging::Translate, VirtAddr};
 
 entry_point!(kernel_main);
 
-pub fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     infinity_os::init();
 
     shell::print_banner();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { kernel::memory::init(phys_mem_offset) };
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        print!("{:?} -> {:?}\n", virt, phys);
+    }
+
     shell::print_prompt();
 
     infinity_os::hlt_loop();
